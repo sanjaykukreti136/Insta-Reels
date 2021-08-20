a35f801cf0637c16e287727f6fb4617160498d7c
Sanjay <sanjaykukreti900@gmail.com>
2021-08-20 11:55:58 +0530

    database connected

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -1,18 +1,37 @@
-import { useContext } from "react";
-import { auth } from "../firebase";
+import { useContext, useEffect, useState } from "react";
+import { auth, firestore, storage } from "../firebase";
 
 import { authContext } from "../AuthProvider";
-import { Redirect } from "react-router-dom";
+import { BrowserRouter, Redirect } from "react-router-dom";
 import "./Home.css"
 import Videocard from "./Videocard"
+import { logDOM } from "@testing-library/react";
 let Home = () => {
   let user = useContext(authContext);
+  let [posts, setpost] = useState([])
+  useEffect(() => {
+    console.log("x");
+    firestore.collection("posts").onSnapshot((querySnapshot) => {
+      let docsArr = querySnapshot.docs;
+      console.log("x");
+      let arr = []
+      for (let i = 0; i < docsArr.length; i++) {
+        console.log(docsArr[i].data())
+        arr.push({ id: docsArr[i].id, ...docsArr[i].data() })
+      }
+      setpost(arr);
+    })
+  }, [])
+
   return (
     <>
       {user ? "" : <Redirect to="/login" />}
 
       <div className="video-container" >
-        <Videocard />
+        {posts.map((el) => {
+          return <Videocard key={el.id} data={el} />
+        })}
+
       </div>
       <button
         className="home-logout-btn"
@@ -22,6 +41,40 @@ let Home = () => {
       >
         lOGOUT
       </button>
+      <input
+        onClick={(e) => {
+          e.currentTarget.value = null;
+        }}
+        type="file" onChange={(e) => {
+          let file = e.currentTarget.files[0];
+          let { name, size, type } = file;
+
+          size = size / 1000000;
+          if (size > 10) {
+            alert("size limit exceeds");
+            return;
+          }
+          type = type.split("/")[0];
+          if (type != "video") {
+            alert("not video");
+            return;
+          }
+
+          let uploadTask = storage.ref(`/posts/${user.uid}/${Date.now() + name}`).put(file)
+          uploadTask.on("state_changed", null, null, () => {
+            uploadTask.snapshot.ref.getDownloadURL().then((url) => {
+              console.log(url);
+              firestore
+                .collection("posts")
+                .add({ name: user.displayName, url, like: [], comment: [] })
+            }).then(() => {
+              window.alert("New Posts");
+              window.location.reload(false);
+            }
+            )
+          })
+
+        }} />
     </>
   );
 };
--- src/firebase.js
+++ src/firebase.js
@@ -2,7 +2,7 @@ import firebase from "firebase/app";
 import config from "./config.json";
 import "firebase/auth";
 import "firebase/firestore";
-
+import "firebase/storage"
 firebase.initializeApp(config);
 
 //flag => using google
@@ -12,7 +12,7 @@ let provider = new firebase.auth.GoogleAuthProvider();
 export const auth = firebase.auth();
 
 export const firestore = firebase.firestore();
-
+export const storage = firebase.storage();
 export const signInWithGoogle = () => {
   //ki muje popup ko use krke sign up krna with google
   auth.signInWithPopup(provider);
